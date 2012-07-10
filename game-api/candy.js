b7ae1e47d237840ead18427d98067458a9d8b789
Jonatan Männchen <jonatan.maennchen@amiadogroup.com>
2012-07-10 14:48:38 +0200

    implemented the first steps of the api

--- game-api/candy.js
+++ game-api/candy.js
@@ -23,5 +23,57 @@ CandyShop.GameAPI = (function(self, Candy, $) {
 		version: '0.1.1'
 	};
 	
+	/** Object: games
+	 * Holds Opend Games
+	 * 
+	 * Contains List:
+	 *   (Object) room - A Game
+	 */
+	self.games = {};
+	
+	/** Function: init
+	 * Initializes the game-api plugin with the default settings.
+	 */
+	self.init = function(){
+		Candy.View.Event.Message.beforeShow = this.handleBeforeShow;
+	};
+	
+	/** Function: handleBeforeShow
+	 * Looks into message, if message contains
+	 * api-stuff, hide and handle it.
+	 * 
+	 * Parameters:
+	 *   (Array) args
+	 */
+	self.handleBeforeShow = function(args) {
+		if(args.message.substring(0, 9) == 'game-api:') {
+			try {
+				CandyShop.GameAPI.apiCall(args.roomJid, $.parseJSON(args.message.substring(9)));
+			 } catch (e) {
+				 console.log(e);
+				 return args.message;
+			 }
+			return null;
+		} else {
+			return args.message;
+		}
+	};
+	
+	/** Function: apiCall
+	 * Make an API Call
+	 * 
+	 * Parameters:
+	 *   (String) roomJid
+	 *   (Array) data
+	 */
+	self.apiCall = function(roomJid, data) {
+		if(typeof this.games[roomJid] != 'undefined') {
+			this.games[roomJid].apiCall(data);
+		} else {
+			// Not implemented for now....
+			console.log('Should do an Invite now...');
+		}
+	};
+	
 	return self;
 }(CandyShop.GameAPI || {}, Candy, jQuery));
